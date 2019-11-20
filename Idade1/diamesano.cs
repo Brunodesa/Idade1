c94ccbe3e712a6b66bf219b16756daea8cb334d5
BD1 <brunodesa288@gmail.com>
2019-11-20 13:14:13 +0000

    Adicionar arquivos de projeto.

---
+++ Idade1/diamesano.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idade1
+{
+    class diamesano
+    {
+        private DateTime data = new DateTime().Date;
+
+
+        public string recebe
+        {
+            set
+            {
+                data = Convert.ToDateTime(value);
+
+            }
+        }
+
+        public string envia
+        {
+            get
+            {
+                DateTime today = DateTime.Now;
+
+                int anos = today.Year - data.Year;
+                int meses = today.Month - data.Month;
+                int dias = today.Day - data.Day;
+                if (meses < 0)
+                {
+                    anos--;
+                    meses++;
+                    dias +=;
+                }
+
+                string frase = "A sua idade é";
+                frase += anos.ToString() + "anos";
+                frase += anos.ToString() + "meses";
+                frase += anos.ToString() + "dias";
+
+            }
+
+        }
+
+    }
+}
